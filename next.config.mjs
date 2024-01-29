137498eafb36a6c06981ee0cc706a30e4d5cf91a
SiAce <siacespark@gmail.com>
2024-01-28 17:44:37 -0800

    Add redirect for home page

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/converter/base64',
+        permanent: true,
+      },
+    ]
+  }
+};
 
 export default nextConfig;
