27710617943c2791c48d6a8047e830d03f82cd89
SiAce <siacespark@gmail.com>
2024-03-07 10:53:38 -0800

    Improve font and color

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,19 @@
 import { Analytics } from "@vercel/analytics/react";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 import type { Metadata } from "next";
-import { Roboto } from "next/font/google";
+import { Roboto as FontSans } from "next/font/google"
 
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
 import { TailwindIndicator } from "@/components/tailwind-indicator";
 import { Toaster } from "@/components/ui/toaster";
+import { cn } from "@/lib/utils";
 
-const roboto = Roboto({
+const fontSans = FontSans({
   subsets: ["latin"],
   weight: ["100", "300", "400", "500", "700", "900"],
-});
+  variable: "--font-sans",
+})
 
 export const metadata: Metadata = {
   title: "Web Tool Kit",
@@ -32,7 +34,10 @@ export default function RootLayout({
           crossOrigin="anonymous"
         ></script>
       </head>
-      <body className={roboto.className}>
+      <body className={cn(
+        "min-h-screen bg-background font-sans antialiased",
+        fontSans.variable
+      )}>
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
           {children}
           <Analytics />
--- src/components/converter/converter-page.tsx
+++ src/components/converter/converter-page.tsx
@@ -17,7 +17,7 @@ export default function ConverterPage({
   return (
     <main className="relative py-6 lg:gap-10 lg:py-10 xl:grid xl:grid-cols-[1fr_300px]">
       <div className="mx-auto w-full min-w-0">
-        <h1 className="scroll-m-20 text-4xl font-bold tracking-tight lg:text-5xl">
+        <h1 className="scroll-m-20 text-4xl font-bold tracking-tight lg:text-5xl font-sans">
           {pageName}
         </h1>
         <TabbedConverter
